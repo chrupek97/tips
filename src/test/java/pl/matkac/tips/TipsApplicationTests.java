01397f7bbb66a8196ed544d8d5e054edf57e3741
chrupek97 <mateusz.rachubka@vp.pl>
2021-09-26 13:18:45 +0200

    Init project

---
+++ src/test/java/pl/matkac/tips/TipsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matkac.tips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
