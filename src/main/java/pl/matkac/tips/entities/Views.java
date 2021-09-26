c21b237f7044b95d0edb500e4954bfe1f36feaf6
kacpyr23 <49038601+kacpyr23@users.noreply.github.com>
2021-09-26 15:46:03 +0200

    Add JsonView file

---
+++ src/main/java/pl/matkac/tips/entities/Views.java
@@ -0,0 +1,9 @@
+package pl.matkac.tips.entities;
+
+public class Views {
+    public interface UserJson {}
+    public interface TipsJson {}
+    public interface TeamJson {}
+    public interface MatchJson {}
+    public interface LeagueJson {}
+}
